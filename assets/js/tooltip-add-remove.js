1c90a0d8c30a58f437df315fbaa3e8a14688dc02
Viktoriya <kapitonenko.vick@ya.ru>
2019-01-03 12:34:35 +0300

    Change arrow, common tooltip styles

--- assets/js/tooltip-add-remove.js
+++ assets/js/tooltip-add-remove.js
@@ -2,7 +2,7 @@ const heading = document.querySelector('.account-info__username');
 const arrow = heading.querySelector('.account-info__arrow');
 const className = 'account-info__arrow_clicked';
 const tooltip = heading.querySelector('.tooltip__container');
-const tooltipArrow = tooltip.querySelector('.tooltip-bottom__arrow');
+const tooltipArrow = tooltip.querySelector('.tooltip__arrow');
 
 heading.addEventListener('click', function () {
   if (arrow.classList.contains(className)) {
