c57909e1fa7dea09847a54ce4ad9dcbc869930d8
Viktoriya <kapitonenko.vick@ya.ru>
2019-01-03 21:00:45 +0300

    Aside: check sidebar and button

--- assets/js/aside.js
+++ assets/js/aside.js
@@ -5,16 +5,18 @@ const app = document.querySelector('.app');
 const overlay = document.createElement('div');
 overlay.className = 'overlay';
 
-mobileBar.addEventListener('click', function(e) {
-  e.preventDefault;
+if (mobileBar && mobileSidebar) {
+  mobileBar.addEventListener('click', function (e) {
+    e.preventDefault;
+  
+    app.appendChild(overlay);
+    mobileSidebar.style.left = '0';
+  });
 
-  app.appendChild(overlay);
-  mobileSidebar.style.left = '0';
-});
-
-document.addEventListener('click', function(e) {
-  if (!mobileSidebar.contains(e.target) && !(mobileBar.contains(e.target)) && mobileSidebar.hasAttribute('style')) {
-    mobileSidebar.removeAttribute('style');
-    app.removeChild(overlay);
-  }
-});
+  document.addEventListener('click', function (e) {
+    if (!mobileSidebar.contains(e.target) && !(mobileBar.contains(e.target)) && mobileSidebar.hasAttribute('style')) {
+      mobileSidebar.removeAttribute('style');
+      app.removeChild(overlay);
+    }
+  });
+}
