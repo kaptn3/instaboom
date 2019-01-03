6620bce33761a97c9d4e95b88b33bd2f479b87f1
Viktoriya <kapitonenko.vick@ya.ru>
2019-01-03 12:36:48 +0300

    Tooltip for preview subscribe

--- assets/js/tooltip-add-subscribe.js
+++
@@ -1,6 +0,0 @@
-{
-  const btn = document.querySelector('.account-score__btn');
-  const tooltip = document.querySelector('.account-score__tooltip');
-
-  showTooltip(btn, tooltip, 'total-tooltip__close');
-}
---
+++ assets/js/tooltip-subscribe.js
@@ -0,0 +1,15 @@
+{
+  const btn = document.querySelector('.account-score__btn');
+  const tooltip = document.querySelector('.account-score__tooltip');
+
+  showTooltip(btn, tooltip, 'total-tooltip__close');
+}
+
+{
+  const tooltip = document.querySelector('.add-subscribe-form__tooltip');
+  
+  const btn = document.querySelector('.add-subscribe-form__preview-btn');
+  const tooltipContainer = tooltip.querySelector('.tooltip__container');
+
+  showTooltip(btn, tooltipContainer, 'tooltip__close');
+}
