1383603166703887e1f6d213d38124bd4e6b1ede
Viktoriya <kapitonenko.vick@ya.ru>
2019-01-24 18:56:40 +0300

    Fix touch buttons for Safari; fix aside overlay click in ios

--- assets/js/aside.js
+++ assets/js/aside.js
@@ -2,7 +2,8 @@ const mobileBar = document.querySelector('.header__mobile-menu');
 const mobileSidebar = document.querySelector('.aside');
 const app = document.querySelector('.app');
 
-const overlay = document.createElement('div');
+const overlay = document.createElement('button');
+overlay.setAttribute('title', 'Закрыть меню');
 overlay.className = 'overlay';
 
 if (mobileBar && mobileSidebar) {
@@ -23,20 +24,21 @@ if (mobileBar && mobileSidebar) {
 
 {
   const divGift = document.querySelector('.aside__gift');
-  if (document.body.clientWidth > 768) {
-    divGift.style.top = Math.max(286, 286 + (0 - (646 - window.innerHeight))) + 'px';
-  } else {
-    divGift.style.top = Math.max(286, 286 + (0 - (523 - window.innerHeight))) + 'px';
-  }
-
-  window.onscroll = function () {
-    let scrolled = window.pageYOffset || document.documentElement.scrollTop;
-    let windowHeight = window.innerHeight;
+  if (divGift) {
     if (document.body.clientWidth > 768) {
-      // 286 - высота блока с меню, 646 - высота меню + высота баннера
-      divGift.style.top = Math.max(286, 286 + (scrolled - (646 - windowHeight))) + 'px';
+      divGift.style.top = Math.max(286, 286 + (0 - (646 - window.innerHeight))) + 'px';
     } else {
-      divGift.style.top = Math.max(286, 286 + (scrolled - (523 - windowHeight))) + 'px';
+      divGift.style.top = Math.max(286, 286 + (0 - (523 - window.innerHeight))) + 'px';
+    }
+    window.onscroll = function () {
+      let scrolled = window.pageYOffset || document.documentElement.scrollTop;
+      let windowHeight = window.innerHeight;
+      if (document.body.clientWidth > 768) {
+        // 286 - высота блока с меню, 646 - высота меню + высота баннера, 360 - высота баннера
+        divGift.style.top = Math.max(286, Math.min(286 + (scrolled - (646 - windowHeight)), (document.body.offsetHeight - 360))) + 'px';
+      } else {
+        divGift.style.top = Math.max(286, Math.min(286 + (scrolled - (523 - windowHeight)), (document.body.offsetHeight - 360))) + 'px';
+      }
     }
   }
 }
