99a6d2002dc4100177595f3b792e73326c4f20a7
Viktoriya <kapitonenko.vick@ya.ru>
2019-02-12 18:25:40 +0300

    Aside: change fixing sidebar

--- assets/js/aside.js
+++ assets/js/aside.js
@@ -21,24 +21,3 @@ if (mobileBar && mobileSidebar) {
     }
   });
 }
-
-{
-  const divGift = document.querySelector('.aside__gift');
-  if (divGift) {
-    if (document.body.clientWidth > 768) {
-      divGift.style.top = Math.max(286, 286 + (0 - (646 - window.innerHeight))) + 'px';
-    } else {
-      divGift.style.top = Math.max(286, 286 + (0 - (523 - window.innerHeight))) + 'px';
-    }
-    window.onscroll = function () {
-      let scrolled = window.pageYOffset || document.documentElement.scrollTop;
-      let windowHeight = window.innerHeight;
-      if (document.body.clientWidth > 768) {
-        // 286 - высота блока с меню, 646 - высота меню + высота баннера, 360 - высота баннера
-        divGift.style.top = Math.max(286, Math.min(286 + (scrolled - (646 - windowHeight)), (document.body.offsetHeight - 360))) + 'px';
-      } else {
-        divGift.style.top = Math.max(286, Math.min(286 + (scrolled - (523 - windowHeight)), (document.body.offsetHeight - 360))) + 'px';
-      }
-    }
-  }
-}
